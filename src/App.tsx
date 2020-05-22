0cda8c3b2363366464777f0176c7945553cbcdec
Mo Jiwa <mohammed.jiwa@gmail.com>
2020-05-23 00:42:03 +0100

    change to react pure component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { Filter } from './enums';
 import Button from './Button';
 
-function App() {
-  const [themeSwitch, setThemeSwitch] = useState(Filter.Dark);
+interface IAppProps {
 
-  return (
-    <div className={`${themeSwitch === Filter.Dark ? 'bg-gray-900 text-gray-300' : 'bg-gray-300 text-gray-900'} h-screen`}>      
-      <div className='right-0 fixed mr-1 mt-1'>
-        <Button
-          onClick={() => setThemeSwitch(Filter.Light)}
-          text="Light"
-          customClass={`bg-gray-400 mr-1 text-gray-900 rounded-l-lg w-16 ${themeSwitch === Filter.Light ? 'hover:shadow-none' : 'hover:bg-gray-200'}`}/>
-        <Button
-          onClick={() => setThemeSwitch(Filter.Dark)}
-          text="Dark"
-          customClass={`bg-gray-800 text-gray-300 rounded-r-lg w-16 ${themeSwitch === Filter.Dark ? 'hover:shadow-none' : 'hover:bg-gray-900'}`} />
+}
+
+interface IAppState {
+  ThemeSwitch: Filter;
+}
+
+class App extends React.PureComponent<IAppProps, IAppState> {
+  state = {
+    ThemeSwitch: Filter.Dark
+  }
+
+  render() {
+    return (
+      <div className={`${this.state.ThemeSwitch === Filter.Dark ? 'bg-gray-900 text-gray-300' : 'bg-gray-300 text-gray-900'} h-screen`}>      
+        <div className='right-0 fixed mr-1 mt-1'>
+          <Button
+            onClick={() => { this.setState({ThemeSwitch: Filter.Light })}}
+            text="Light"
+            customClass={`bg-gray-400 mr-1 text-gray-900 rounded-l-lg w-16 ${this.state.ThemeSwitch === Filter.Light ? 'hover:shadow-none' : 'hover:bg-gray-200'}`}/>
+          <Button
+            onClick={() => { this.setState({ThemeSwitch: Filter.Dark })}}
+            text="Dark"
+            customClass={`bg-gray-800 text-gray-300 rounded-r-lg w-16 ${this.state.ThemeSwitch === Filter.Dark ? 'hover:shadow-none' : 'hover:bg-gray-900'}`} />
+        </div>
+        <div className='mx-auto text-5xl center-content text-center'>
+          <span>Hi, I'm <span className='italic'>Mo Jiwa</span>. </span>
+          <br/><span>I'm a developer.</span>
+        </div>
       </div>
-      <div className='mx-auto text-5xl center-content text-center'>
-        <span>Hi, I'm <span className='italic'>Mo Jiwa</span>. </span>
-        <br/><span>I'm a developer.</span>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;

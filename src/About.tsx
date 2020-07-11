dd33b09840b77fc3f72691c67d1323331937d912
Mo Jiwa <mohammed.jiwa@gmail.com>
2020-07-11 20:25:13 +0100

    Update font for mobile in About

--- src/About.tsx
+++ src/About.tsx
@@ -70,7 +70,7 @@ export default class About extends React.PureComponent<IAboutProps, {}> {
           </div>            
           <div className='w-2/3 md:w-1/2 md:flex justify-between'>
             <div className='animated-list mt-4 ml-2'>
-              <label className='transition-colors duration-300 hover:text-teal-600 text-base md:text-xl ml-4 border-teal-600 border-l-4 px-4'>Skills</label>
+              <label className='transition-colors duration-300 hover:text-teal-600 text-sm md:text-xl ml-4 border-teal-600 border-l-4 px-4'>Skills</label>
               <ul>                
                 {LIST_OF_SKILLS.sort().map((skill: string) =>
                   <li className='ml-6' key={skill}><label>{skill}</label></li>
@@ -78,7 +78,7 @@ export default class About extends React.PureComponent<IAboutProps, {}> {
               </ul>
             </div>  
             <div className='animated-list mt-4 ml-2'>
-              <label className='transition-colors duration-300 hover:text-teal-600 text-base md:text-xl ml-4 border-teal-600 border-l-4 px-4'>Weapons of Choice</label>
+              <label className='transition-colors duration-300 hover:text-teal-600 text-sm md:text-xl ml-4 border-teal-600 border-l-4 px-4'>Weapons of Choice</label>
               <ul>
                 {LIST_OF_TOOLS.sort().map((tool: string) =>
                   <li className='ml-6' key={tool}><label>{tool}</label></li>

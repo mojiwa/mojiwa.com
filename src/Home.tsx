49e8155ca59b85fdc6ef9ae33fc3420f8807c291
Mo Jiwa <mohammed.jiwa@gmail.com>
2020-07-31 20:33:03 +0100

    Set homepage font larger for mobile

--- src/Home.tsx
+++ src/Home.tsx
@@ -13,17 +13,17 @@ export default class Home extends React.PureComponent<IHomeProps, {}> {
     render() {
         return(
             <div id='home-page'>           
-                <div className={`font-thin center-content text-center cursor-default`} >
-                    <span className='text-xl md:text-5xl'>Hi, I'm <span className='font-normal text-teal-600'>Mo Jiwa</span>.</span>
+                <div className={`w-full p-2 font-thin center-content text-center cursor-default`} >
+                    <span className='text-4xl md:text-5xl'>Hi, I'm <span className='font-normal text-teal-600'>Mo Jiwa</span>.</span>
                     <br />
-                    <span className='text-lg md:text-3xl'>I'm a full-stack web and software developer</span>
+                    <span className='text-2xl md:text-3xl'>I'm a full-stack web and software developer</span>
                     <div>
                         <Link to='/about'>
                             <button      
                                 onClick={() => this.props.SetPage(Page.About)}                 
                                 onMouseOver={() => document.getElementById('see-more-svg')?.classList.add('rotate-svg')}     
                                 onMouseLeave={() => document.getElementById('see-more-svg')?.classList.remove('rotate-svg')}  
-                                className={`see-more-button mt-4 focus:outline-none font-thin text-md md:text-2xl rounded-lg pt-1 pb-1 pl-4 pr-4 hover:bg-teal-600 border-solid border-2 hover:text-gray-300 hover:border-teal-600 ${this.props.Theme === Theme.Dark ? 'text-gray-300 border-gray-300' : 'text-gray-900 border-gray-900'}`}>
+                                className={`see-more-button mt-4 focus:outline-none font-thin text-xl md:text-2xl rounded-lg pt-1 pb-1 pl-4 pr-4 hover:bg-teal-600 border-solid border-2 hover:text-gray-300 hover:border-teal-600 ${this.props.Theme === Theme.Dark ? 'text-gray-300 border-gray-300' : 'text-gray-900 border-gray-900'}`}>
                                     See More  <svg id='see-more-svg' className='inline transition-transform duration-300' height="20px" width="20px" fill="currentColor" viewBox="0 0 20 20">
                                                 <path d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" fillRule="evenodd" />
                                             </svg>
